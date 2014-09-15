520b891d7d0ebed0a1f8d271d77f35b8090de2cc
sunchongyang1 <suncyqq@gmail.com>
2014-09-15 19:22:32 +0800

    Merge origin/master
    Conflicts:
    	.gitignore

--- CommonInfra-war/src/java/CommonManagedBean/LoginManagedBean.java
+++ CommonInfra-war/src/java/CommonManagedBean/LoginManagedBean.java
@@ -6,20 +6,29 @@
 
 package CommonManagedBean;
 
+import Common.entity.Account;
 import Common.session.AccountManagementSessionBeanLocal;
+import java.io.IOException;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import util.exception.AccountTypeNotExistException;
+import util.exception.PasswordNotMatchException;
+import util.exception.UserNotExistException;
 
 
 /**
  *
  * @author Sun Mingjia
  */
+@ManagedBean
+@RequestScoped
 public class LoginManagedBean {
     
-    private String domain;
+    private String userType;
     private String username;
     private String password;
     private int attempts;
@@ -33,28 +42,42 @@ public class LoginManagedBean {
         attempts=0;
     }
     
-    public void login() {
+    public void login() throws PasswordNotMatchException, IOException, AccountTypeNotExistException {
         System.err.println("login");
         
         //set the session attribute
-        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("domain", domain);
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("userType", userType);
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("username", username);
         
         try {
-            if ((domain!=null)&&(username!=null)&&(password!=null)) {
-                boolean check=amsbl.checkLogin(username, domain, password);
+            if ((userType!=null)&&(username!=null)&&(password!=null)&&(amsbl.checkDelete(username,userType)==false)&&(amsbl.checkLock(username,userType)==false)&&attempts<max_attempts) {
+                boolean check=amsbl.checkLogin(username, userType, password);
                 if (check==true) {
                     ec.redirect("workspaceURL");
                 }
                 if (check==false) {
-                    this.displayFaceMessage("User "+username+" does not exist!");
+                    attempts++;
+                    if (max_attempts-attempts>0) {
+                    this.displayFaceMessage("You have "+(max_attempts-attempts)+"try before your account is frozen.");
+                    }
                 }
+            }
+            else if (amsbl.checkDelete(username,userType)) {
+                this.displayFaceMessage("User "+username+"does not exist");
+            }
+            else if (amsbl.checkLock(username,userType)) {
+                this.displayFaceMessage("Account has been locked. Please approach system admin for activation.");
+            }
+            else if (attempts==max_attempts) {
+                Account account=amsbl.retrieveAccount(username, userType);
+                amsbl.deactivateAccount(account.getId(), userType);
             }
             else {
                 this.displayFaceMessage("Please enter valid input for all fields.");
             }
         }
-        catch (Exception ex) {          
+        catch (UserNotExistException ex) {
+            this.displayFaceMessage("User "+username+"does not exist");
         }
     }
 
@@ -64,11 +87,11 @@ public class LoginManagedBean {
     }
 
     public String getDomain() {
-        return domain;
+        return userType;
     }
 
-    public void setDomain(String domain) {
-        this.domain = domain;
+    public void setDomain(String userType) {
+        this.userType = userType;
     }
 
     public String getUsername() {
@@ -93,7 +116,5 @@ public class LoginManagedBean {
 
     public void setAttempts(int attempts) {
         this.attempts = attempts;
-    }
-    
-    
+    }   
 }
